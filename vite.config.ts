f8d913b91d4518b8bd3ccbfbb18820f9d25139b1
eslava-ai <introduce-el-email-de-eslava-ai@example.com>
2025-11-30 22:21:02 +0100

    Explicitly configure Vite build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      build: {
+        outDir: 'dist',
+        emptyOutDir: true
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
